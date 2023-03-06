426c03ab573a9b05c63c727bc21869849e5bf3a6
akhmedov04 <119047485+akhmedov04@users.noreply.github.com>
2023-03-06 15:50:05 +0500

    Update urls.py

--- userapp/urls.py
+++ userapp/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('login/', LoginView.as_view()),
-    path('register/', RegisterView.as_view())
+    path('register/', RegisterView.as_view()),
+    path('logout/', logoutview)
 ]
